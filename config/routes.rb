a0ea68fc32db2eb19884903d080e3a3423d2d6ea
tsutaya-raven <raven@yoshikawatatsuyanoMacBook-Air.local>
2019-03-01 15:31:21 +0900

    Edit.scss&add.group
    CSSの調整とグループ機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
 
-devise_for :users
-  root 'messages#index'
-  resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
+  # devise_for :users
+  # root 'groups#index'
+  # resources :users, only: [:index, :edit, :update]
+  # resources :groups, only: [:index,:new, :create, :edit, :update] do
+  #   resources :messages, only: [:index, :create]
+  # end
+
+    devise_for :users
+  root 'groups#index'
+  resources :users, only: [:edit, :update, :index]
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+    resources :users, only: [:index]
   end
 end
+
